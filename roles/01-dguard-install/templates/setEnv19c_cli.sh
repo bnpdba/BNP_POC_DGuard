ce1660eb3f5500e3df04d0354a2959fc79589753
Systems - & Databases-services <LIST.MIDDLEWARESQUADUNIXLINUXDATABASES@bnpparibasfortis.com>
2021-11-06 14:41:13 +0100

    Delete roles directory

--- roles/01-dguard-install/templates/setEnv19c_cli.sh
+++
@@ -1,9 +0,0 @@
-# Oracle Settings
-export TMP=/tmp
-export TMPDIR=$TMP
-export ORACLE_BASE=/opt/oracle
-export ORACLE_HOME=$ORACLE_BASE/instantclient_19_10
-export LD_LIBRARY_PATH=$ORACLE_HOME:/lib:/usr/lib
-export ANSIBLE_CONFIG=/home/ansible/BNP_POC_DGuard/ansible_cfg/ansible.cfg
-export TNS_ADMIN=$ORACLE_HOME/network/admin
-export PATH=$ORACLE_HOME:/usr/sbin:/usr/local/bin:$PATH
